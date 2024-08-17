0bbf66bf563e1c4608f3d27f75672f99a9e2128c
hitaex <aseadazeez33@gmail.com>
2024-08-17 06:50:06 +0300

    initial commit

---
+++ toralize.c
@@ -0,0 +1,9 @@
+#include "toralize.h"
+int main(int argc, char *argv[]){
+    char* host;
+    int port; 
+    int(argc < 3) {
+        fprintf(stderr, "USAGE %s <host> <port>", argv[0]);
+        return -1
+    }
+}
---
+++ toralize.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#define PROXY "127.0.0.1"
+#define PROXYPORT 9050
+// 8 bit 
+typedef unsigned char int8 ;
+// 16 bit
+typedef short int int16 ;
+//32 bit
+typedef unsigned int32 ;
+/*      +----+----+----+----+----+----+----+----+----+----+....+----+
+		| VN | CD | DSTPORT |      DSTIP        | USERID       |NULL|
+		+----+----+----+----+----+----+----+----+----+----+....+----+
+ # of bytes:	   1    1      2              4           variable       1*/
+struct proxy_req {
+    int8 vn;
+    int8 cd;
+    int16 dstport;
+    int32 dstip;
+    unsigned char userid[8];
+};
+
